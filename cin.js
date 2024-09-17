0ca11c5a9fd7c2e20095244816f1a581dc5ad093
Anilkumar189 <yandrapuanil143@gmail.com>
2024-09-17 15:41:34 -0400

    first commit

---
+++ cin.js
@@ -0,0 +1,23 @@
+var x=[
+    { seatNumber:[1,2,3,4,5,6,7,8,9,10,11,12], seatName: 'A1', status: 'available', price: 20 },
+    { seatNumber: 2, seatName: 'A2', status: 'available', price: 20 },
+    { seatNumber: 3, seatName: 'A3', status: 'available', price: 20 },
+    { seatNumber: 4, seatName: 'A4', status: 'available', price: 20 },
+    { seatNumber: 5, seatName: 'A5', status: 'available', price: 20 },
+    { seatNumber: 6, seatName: 'A6', status: 'available', price: 20 },
+    { seatNumber: 7, seatName: 'A7', status: 'available', price: 20 },
+    { seatNumber: 8, seatName: 'A8', status: 'available', price: 20 },
+    { seatNumber: 9, seatName: 'A9', status: 'available', price: 20 },
+    { seatNumber: 10, seatName: 'A10', status: 'available', price: 20 },
+    // More seats in row A...
+    { seatNumber: 191, seatName: 'D11', status: 'available', price: 10 },
+    { seatNumber: 192, seatName: 'D12', status: 'available', price: 10 },
+    { seatNumber: 193, seatName: 'D13', status: 'available', price: 10 },
+    { seatNumber: 194, seatName: 'D14', status: 'available', price: 10 },
+    { seatNumber: 195, seatName: 'D15', status: 'available', price: 10 },
+    { seatNumber: 196, seatName: 'D16', status: 'available', price: 10 },
+    { seatNumber: 197, seatName: 'D17', status: 'available', price: 10 },
+    { seatNumber: 198, seatName: 'D18', status: 'available', price: 10 },
+    { seatNumber: 199, seatName: 'D19', status: 'available', price: 10 },
+    { seatNumber: 200, seatName: 'D20', status: 'available', price: 10 }
+];
